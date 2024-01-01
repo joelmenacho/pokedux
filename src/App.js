07722b8575a02b46a2dcd11a83878b97d34929fd
joelmenacho01~ <joelmenacho@gmail.comm>
2023-12-31 22:53:35 -0500

    inicio de desarollo de pokedux

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import {Col} from 'antd';
+import Searcher from './components/Searcher';
+import './App.css';
+
+function App() {
+  return (
+  <div className="App"> 
+  <Col span={8} offset={8}>
+  <Searcher/> 
+  </Col>
+  </div>
+  );
+}
+
+export default App;
