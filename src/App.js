822cbb357ce37c00454e10cce6ebe05e0344d1a4
joelmenacho01~ <joelmenacho@gmail.comm>
2024-01-01 10:05:45 -0500

    update componets

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import {Col} from 'antd';
 import Searcher from './components/Searcher';
 import PokemonList from './components/PokemonList';
+import logo from'./statics/logo.svg';
 import './App.css';
 
 function App() {
   return (
   <div className="App"> 
+  <Col span={4} offset={10}>
+    <img src={logo} alt="pokedux" />
+  </Col>
   <Col span={8} offset={8}>
   <Searcher/> 
   </Col>
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,12 +1,14 @@
+import {StarOutlined} from '@ant-design/icons'
 import{Card} from 'antd';
 import Meta from 'antd/lib/card/Meta';
+import './PokemonList.css';
 
 const PokemonCard =()=>{
     return(
-        <Card style={{ width: 250 }} 
+        <Card 
         title="Ditto"
         cover={<img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/132.png' alt='Ditto' />}
-
+        extra={<StarOutlined />}
       >
         <Meta description='fire, magic'/>
         </Card>
